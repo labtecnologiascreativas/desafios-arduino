45dbf5b2810c49b596d73025d415c65cab034ec6
Clodo <claudiobidau@gmail.com>
2015-10-20 16:07:59 -0300

    primer commit

---
+++ Motor/Motor.ino
@@ -0,0 +1,24 @@
+#include <MotorShield.h>
+
+const int ledPin = 13;
+MS_DCMotor motor(MOTOR_A);
+
+void setup() {
+  pinMode(ledPin,OUTPUT);
+  
+  motor.run(BRAKE);
+  motor.setSpeed(255);
+}
+
+void loop() {
+  motor.run(BACKWARD|RELEASE);
+  delay(3000);
+  motor.run(BRAKE);
+  
+  delay(1000);
+    
+  motor.run(FORWARD|RELEASE);
+  delay(3000);
+  motor.run(BRAKE);
+}
+
