f05770894d03aa0b7f279aaf39b83e97eb719aac
Damian Mantuano <smarbos@gmail.com>
2018-12-11 12:53:33 -0300

    [DM] Agregue explicacion de porque utilizo una funcion

--- Blink/Blink2/Blink2.ino
+++ Blink/Blink2/Blink2.ino
@@ -1,9 +1,9 @@
 /*
   Blink
-  Prende un LED por un segundo, luego lo apaga por otro segundo, y lo repite. 
+  Prende un LED por un segundo, luego lo apaga por otro segundo, y lo repite.
   Este código de ejemplo es de dominio público
  */
- 
+
 // El Pin 13 tiene un LED conectado en la mayoría de las placas Arduino
 // Le ponemos un nombre, en este caso "led":
 int ledVerde = 5;
@@ -12,14 +12,23 @@ int ledAzul = 3;
 int ledAmarillo = 2;
 
 // Aqui escribimos la rutina inicial:
-void setup() {                
+void setup() {
   // inicializamos el pin como salida (OUTPUT en inglés)
-  pinMode(ledVerde, OUTPUT);   
+  pinMode(ledVerde, OUTPUT);
   pinMode(ledRojo, OUTPUT);
   pinMode(ledAzul, OUTPUT);
-  pinMode(ledAmarillo, OUTPUT);  
+  pinMode(ledAmarillo, OUTPUT);
 }
 
+/*
+  Vamos a utilizar una funcion de forma tal de poder reutilizar el codigo.
+  Nuestra funcion recibira 3 parametros:
+  - El led que queremos hacer parpadear
+  - Cuanto tiempo dura encendido (milisegundos)
+  - Cuanto tiempo dura apagado (milisegundos)
+  De esta forma podemos llamar a la misma funcion con diferentes parametros para obtener diferentes resultados.
+  Si no utilizaramos esta funcion, deberiamos repetir las 4 lineas de codigo por cada led.
+*/
 void parpadear(int led, int tiempoPrendido, int tiempoApagado) {
   digitalWrite(led, HIGH);   // Prende el LED (HIGH es "prendido")
   delay(tiempoPrendido);               // espera un segundo
@@ -28,6 +37,7 @@ void parpadear(int led, int tiempoPrendido, int tiempoApagado) {
 }
 
 // Aquí escribimos lo que se va a repetir:
+//  Vamos a llamar a nuestra funcion "parpadear" variando el parametro LED cada vez.
 void loop() {
   parpadear(ledVerde, 250, 250);
   parpadear(ledRojo, 250, 250);
