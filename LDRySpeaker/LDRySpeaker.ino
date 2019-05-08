2cfafafecef1bde5d283e9c9b7de1c0528786797
Charlie <charlie@camba.coop>
2019-05-08 17:39:43 -0300

    se agrego posibilidad de ajustar los valores del ldr

--- LDRySpeaker/LDRySpeaker.ino
+++ LDRySpeaker/LDRySpeaker.ino
@@ -11,6 +11,12 @@ int sensorLDR = A1; // Donde conectaremos el sensor LDR.
 int valorSensorLDR = 0; // Almacena el ultimo valor leido del sensor.
 int notaAReproducir = 0;// Almacen el ultimo valor de nota a reproducir.
 int tempo = 300; // Tiempo de espera entre una nota y la siguiente.
+
+// Variables para  si es necesario ajustar la lectura del sensor ,para realizar esto se debe observar el valor cuando el LDR no tiene luz(tapandolo)
+// y al aplicarle la mayor cantidad posible(sea con un celular o linterna)
+int valorMinimoDeLDR = 440; // Es el valor que se lee del sensor LDR cuando tiene la maxima cantidad de luz
+int valorMaximoDeLDR = 850; // Es el valor que se lee del sensor LDR cuando no tiene luz
+
 // Vamos a armar un Array, que se diferencia del resto de las variables que vimos antes, por contener no uno, sino varios valores.
 // Para indicar que nuestra variable es un array, le agregamos los corchetes ("[]") al final del nombre.
 // Nuestro array va a contener 7 notas, DO, RE, MI, FA, SOL, LA y SI.
@@ -28,20 +34,18 @@ void loop() {
   valorSensorLDR = analogRead(sensorLDR);
   // imprimo el valor en el monitor serial
   Serial.println(valorSensorLDR);
-  
-  // mapeo el valor del sensorLDR a valores entre 0 y 7, es decir, la cantidad de notas en el arrayDeNotas.
-  notaAReproducir = map(valorSensorLDR, 380 , 980, 0 , 7); 
 
-  // Si el sensor mide mas de 390
-  if (valorSensorLDR > 380) {
+  // mapeo el valor del sensorLDR a valores entre 0 y 7, es decir, la cantidad de notas en el arrayDeNotas.
+  notaAReproducir = map(valorSensorLDR, valorMinimoDeLDR ,valorMaximoDeLDR, 0 , 8);
+
+  // Si la nota a reproducir no esta dentro del array de notas
+  if (notaAReproducir < 8) {
     // Le decimos al generadorDeTonos que reproduzca la nota correspondiente.
-    generadorDeTonos.play(arrayDeNotas[notaAReproducir]);  
+    generadorDeTonos.play(arrayDeNotas[notaAReproducir]);
   } else {
     // sino, le decimos que haga silencio.
     generadorDeTonos.stop();
   }
-  
-  
   //esperamos antes de tocar la proxima nota
   delay(tempo);
 }
